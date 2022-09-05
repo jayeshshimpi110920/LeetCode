14503573ffa3859063871210e3b8e3495f0d67b3
Jayesh Anil Shimpi <100369017+jayeshshimpi110920@users.noreply.github.com>
2022-09-05 12:19:57 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (66.07%) - LeetHub

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        
+        int val = root->val;
+        
+        int child =0;
+        if(root->left) child+=root->left->val;
+        if(root->right) child+=root->right->val;
+        
+        return val==child;
+    }
+};
